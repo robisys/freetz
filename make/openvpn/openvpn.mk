robisys/freetz

commit c8b23d64f0242f2ee23610397b5d786712fdb2d2

openvpn:  * drop support for openvpn 2.2.x
git-svn-id: file:///var/svn/freetz/trunk@13486 149334a1-2f27-0410-a3b9-fc62619ac1e6

make/openvpn/openvpn.mk
@@ -1,16 +1,15 @@
-$(call PKG_INIT_BIN,$(if $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2),2.2.3,2.3.8))
-$(PKG)_SOURCE_MD5_2.2.3:=19265405cbcbf7c86be81418ecb02f4b
-$(PKG)_SOURCE_MD5_2.3.8:=51d996f1f1fc30f501ae251a254effeb
-$(PKG)_SOURCE_MD5:=$($(PKG)_SOURCE_MD5_$($(PKG)_VERSION))
+$(call PKG_INIT_BIN, 2.3.8)
+$(PKG)_SOURCE_MD5:=51d996f1f1fc30f501ae251a254effeb
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://swupdate.openvpn.net/community/releases
 
-$(PKG)_CONDITIONAL_PATCHES+=$(call GET_MAJOR_VERSION,$($(PKG)_VERSION))
 ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_WITH_TRAFFIC_OBFUSCATION)),y)
 $(PKG)_CONDITIONAL_PATCHES+=$(call GET_MAJOR_VERSION,$($(PKG)_VERSION))/obfuscation
 endif
 
-$(PKG)_BINARY:=$($(PKG)_DIR)/$(if $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2),openvpn,src/openvpn/openvpn)
+$(PKG)_PATCH_POST_CMDS += $(call POLARSSL_HARDCODE_VERSION,12,configure include/*.h src/openvpn/*.h src/openvpn/*.c)
+
+$(PKG)_BINARY:=$($(PKG)_DIR)/src/openvpn/openvpn
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/usr/sbin/openvpn
 
 $(PKG)_STARTLEVEL=81
@@ -21,12 +20,6 @@ $(PKG)_DEPENDS_ON += $(if $(FREETZ_PACKAGE_OPENVPN_OPENSSL),openssl)
 $(PKG)_DEPENDS_ON += $(if $(FREETZ_PACKAGE_OPENVPN_POLARSSL),polarssl12)
 $(PKG)_DEPENDS_ON += $(if $(FREETZ_PACKAGE_OPENVPN_WITH_LZO),lzo)
 
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_VERSION_2_3)),y)
-$(PKG)_PATCH_POST_CMDS += $(call POLARSSL_HARDCODE_VERSION,12,configure include/*.h src/openvpn/*.h src/openvpn/*.c)
-endif
-
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_VERSION_2_2
-$(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_VERSION_2_3
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_OPENSSL
 $(PKG)_REBUILD_SUBOPTS += FREETZ_OPENSSL_SHLIB_VERSION
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_POLARSSL
@@ -39,19 +32,10 @@ $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_OPENVPN_STATIC
 $(PKG)_REBUILD_SUBOPTS += FREETZ_TARGET_IPV6_SUPPORT
 $(PKG)_REBUILD_SUBOPTS += FREETZ_LIB_libpolarssl12_WITH_BLOWFISH
 
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2)),y)
-$(PKG)_EXTRA_LIBS += $(if $(FREETZ_PACKAGE_OPENVPN_OPENSSL),-ldl)
-$(PKG)_CONFIGURE_OPTIONS += --disable-http
-$(PKG)_CONFIGURE_PRE_CMDS += autoreconf -f -i;
-endif
-
-ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_VERSION_2_3)),y)
 $(PKG)_CONFIGURE_OPTIONS += --disable-http-proxy
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_MAKE_AC_VARIABLES_PACKAGE_SPECIFIC,lib_polarssl_ssl_init lib_polarssl_aes_crypt_cbc)
-endif
 
-# 2.3 doesn't support --with-*-path options
 $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_MAKE_AC_VARIABLES_PACKAGE_SPECIFIC,path_IFCONFIG path_IPROUTE path_ROUTE)
 $(PKG)_CONFIGURE_ENV += $(pkg)_path_IFCONFIG=/sbin/ifconfig
 $(PKG)_CONFIGURE_ENV += $(pkg)_path_IPROUTE=/sbin/ip
@@ -62,7 +46,7 @@ $(PKG)_CONFIGURE_PRE_CMDS += find $(abspath $($(PKG)_DIR)) -name Makefile.in -ty
 
 $(PKG)_EXTRA_CFLAGS  += -ffunction-sections -fdata-sections
 $(PKG)_EXTRA_LDFLAGS += -Wl,--gc-sections
-$(PKG)_EXTRA_LDFLAGS += $(if $(FREETZ_PACKAGE_OPENVPN_STATIC),$(if $(FREETZ_PACKAGE_OPENVPN_VERSION_2_2),-static,-all-static))
+$(PKG)_EXTRA_LDFLAGS += $(if $(FREETZ_PACKAGE_OPENVPN_STATIC),-all-static)
 
 $(PKG)_CONFIGURE_OPTIONS += --sysconfdir=/mod/etc/openvpn
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_OPENVPN_WITH_LZO),--enable-lzo,--disable-lzo)

make/openvpn/Config.in
@@ -8,24 +8,6 @@ config FREETZ_PACKAGE_OPENVPN
 
 choice
 	depends on FREETZ_PACKAGE_OPENVPN
-	prompt "Version"
-	default FREETZ_PACKAGE_OPENVPN_VERSION_2_3
-
-	config FREETZ_PACKAGE_OPENVPN_VERSION_2_3
-		bool "2.3.8"
-		help
-			Latest stable version of OpenVPN with support for polarSSL
-
-	config FREETZ_PACKAGE_OPENVPN_VERSION_2_2
-		bool "2.2.3 - old stable version"
-		help
-			Prior stable version of OpenVPN
-			Please use just in case of problems with version 2.3.X
-
-endchoice
-
-choice
-	depends on FREETZ_PACKAGE_OPENVPN
 	prompt "SSL library"
 	default FREETZ_PACKAGE_OPENVPN_OPENSSL
 
@@ -37,7 +19,6 @@ choice
 
 	config FREETZ_PACKAGE_OPENVPN_POLARSSL
 		bool "PolarSSL"
-		depends on FREETZ_PACKAGE_OPENVPN_VERSION_2_3
 		select FREETZ_LIB_libpolarssl12 if ! FREETZ_PACKAGE_OPENVPN_STATIC
 		help
 			Leads to smaller binaries, but lacks support for some openssl features.
@@ -70,7 +51,7 @@ config FREETZ_PACKAGE_OPENVPN_WITH_LZO
 
 config FREETZ_PACKAGE_OPENVPN_WITH_TRAFFIC_OBFUSCATION
 	bool "With traffic obfuscation"
-	depends on FREETZ_PACKAGE_OPENVPN && FREETZ_PACKAGE_OPENVPN_VERSION_2_3
+	depends on FREETZ_PACKAGE_OPENVPN
 	default n
 	help
 		Adds support for an additional configuration option 'scramble'

make/openvpn/patches/2.2/200-no_in6addr_any_in_libc_without_IPv6_fix.patch
@@ -1,26 +1,0 @@
---- pool.c
-+++ pool.c
-@@ -320,7 +320,7 @@
- static struct in6_addr
- ifconfig_pool_handle_to_ipv6_base (const struct ifconfig_pool* pool, ifconfig_pool_handle hand)
- {
--  struct in6_addr ret = in6addr_any;
-+  struct in6_addr ret = IN6ADDR_ANY_INIT;
- 
-   /* IPv6 pools are always INDIV (--linear) */
-   if (hand >= 0 && hand < pool->size_ipv6 )
---- socket.c
-+++ socket.c
-@@ -2603,10 +2603,11 @@
- const char *
- print_in6_addr (struct in6_addr a6, unsigned int flags, struct gc_arena *gc)
- {
-+  static const struct in6_addr my_in6addr_any = IN6ADDR_ANY_INIT;
-   struct buffer out = alloc_buf_gc (64, gc);
-   char tmp_out_buf[64];		/* inet_ntop wants pointer to buffer */
- 
--  if ( memcmp(&a6, &in6addr_any, sizeof(a6)) != 0 || 
-+  if ( memcmp(&a6, &my_in6addr_any, sizeof(a6)) != 0 || 
-        !(flags & IA_EMPTY_IF_UNDEF))
-     {
-       inet_ntop (AF_INET6, &a6, tmp_out_buf, sizeof(tmp_out_buf)-1);

make/openvpn/patches/2.2/210-disable_epoll_poll_use_select.patch
@@ -1,34 +1,0 @@
---- configure.ac
-+++ configure.ac
-@@ -385,7 +385,7 @@
- 		 netinet/in.h netinet/in_systm.h dnl
- 		 netinet/tcp.h arpa/inet.h dnl
- 		 netdb.h sys/uio.h linux/if_tun.h linux/sockios.h dnl
--		 linux/types.h sys/poll.h sys/epoll.h err.h dnl
-+		 linux/types.h err.h dnl
-    )
-    AC_CHECK_HEADERS(net/if.h,,,
- 		 [#ifdef HAVE_SYS_TYPES_H
-@@ -510,7 +510,7 @@
- # Windows use stdcall for winsock so we cannot auto detect these
- m4_define([SOCKET_FUNCS], [socket recv recvfrom send sendto listen dnl
-           accept connect bind select gethostbyname inet_ntoa])
--m4_define([SOCKET_OPT_FUNCS], [setsockopt getsockopt getsockname poll])
-+m4_define([SOCKET_OPT_FUNCS], [setsockopt getsockopt getsockname])
- 
- if test "${WIN32}" = "yes"; then
- 
-@@ -565,13 +565,6 @@
- dnl check libraries
- dnl
- 
--dnl Checking for a working epoll
--AC_CHECKING([for working epoll implementation])
--OLDLDFLAGS="$LDFLAGS"
--LDFLAGS="$LDFLAGS -Wl,--fatal-warnings"
--AC_CHECK_FUNC(epoll_create, AC_DEFINE(HAVE_EPOLL_CREATE, 1, [epoll_create function is defined]))
--LDFLAGS="$OLDLDFLAGS"
--
- dnl ipv6 support
- if test "$PF_INET6" = "yes"; then
-   AC_CHECKING([for struct sockaddr_in6 for IPv6 support])

commit c86daa7c5a6286edd4fd389c6785518fe5aa283a

openvpn:  * fix path to obfuscation patch (broken in r13486)
git-svn-id: file:///var/svn/freetz/trunk@13487 149334a1-2f27-0410-a3b9-fc62619ac1e6

make/openvpn/openvpn.mk
@@ -4,7 +4,7 @@ $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=http://swupdate.openvpn.net/community/releases
 
 ifeq ($(strip $(FREETZ_PACKAGE_OPENVPN_WITH_TRAFFIC_OBFUSCATION)),y)
-$(PKG)_CONDITIONAL_PATCHES+=$(call GET_MAJOR_VERSION,$($(PKG)_VERSION))/obfuscation
+$(PKG)_CONDITIONAL_PATCHES+=obfuscation
 endif
 
 $(PKG)_PATCH_POST_CMDS += $(call POLARSSL_HARDCODE_VERSION,12,configure include/*.h src/openvpn/*.h src/openvpn/*.c)